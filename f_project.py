c481f52d716826bfb7472204cb9df3d92366dbc9
Ghostrecon27 <forrestmcbride@Forrests-MacBook-Pro-2.local>
2014-11-28 20:12:35 -0800

    Made it pretty (*o*)

--- f_project.py
+++ f_project.py
@@ -1,115 +1,153 @@
+from tkinter import *
+from tkinter.scrolledtext import *
+import tkinter.scrolledtext as tkst
 
-#number of drinks 
-def user_name():
-    name=input("What is your name? ")
-    return name
 
-# % alcohol 
-def get_content():
-    a=True
-    drink_info=[] 
-    #for loop instead of a while loop maybe
-    while (a):
-        type_alc=input("What kind of alcohol are you drinking?: ")
-        if (type_alc=="done"):
-            a=False
-            return drink_info
-        print("1 drink = 1 shot(1.5oz) or 1 beer(12oz) or 1 glass(5oz) of wine")
-        drinks=int(input("How many drinks have you had?: "))
-        print("When done entering drinks type 'done' in alcohol type prompt")
-        nest_drink=[type_alc,drinks]
-        drink_info.append(nest_drink)
-    return drink_info
+alch = []
+drinks = []
 
+class application(object):
     
-def total_drinks(drink_info):
-    l=len(drink_info)
-    tot_drinks=0
-    for i in range(l):
-        tot_drinks=tot_drinks+float(drink_info[i][1])
-    return tot_drinks
+    def user_name():
+        ans = e1.get()
+        return ans
+    alch = []
+    def get_alchohol():
+        ans = e4.get()
+        alch.append(ans)
+        return alch
+    def get_drinks():
+        ans = int(e5.get())
+        drinks.append(ans)
+        return drinks
+    
+    def get_alchohol_list():
+        application.get_alchohol()
+        application.get_drinks()
+
+    def get_weight():
+        ans = e2.get()
+        return ans
+
+    def get_gender():
+        ans = e3.get()
+        if (ans == 'Male'):
+            ans = .68
+        if (ans == 'Female'):
+            ans = .55
+        return ans
+
+    def time_since_last_drink():
+        ans = e6.get()
+        return ans
+
+    def widmark_formula(tot_drinks,weight,gender,hours):
+        if gender==1:
+            gender_val=.55
+        else:
+            gender_val=.68
+        bac=(((tot_drinks*.06*100*1.055)/(weight*gender_val))-(.015*hours))
+        return bac
+
+    def advice(bac):
+        if bac>=.08:
+            print("DON'T DRIVE! You will receive a DUI and endanger the public")
+        if bac>=.10:
+            print("You're on the road to sloppiness.")
+        if bac>=.20:
+            print("You're most likely blacked out right now.")
+        if bac>=.25:
+            print("You are at a high risk of choking on your own vomit.")
+        if bac>=.35:
+            print("Coma is possible. Stop drinking.")
+        if bac>=.4:
+            print("You are about become a vegetable and may die due to respiratory arrest.")
+    
+    def receipt(name,bac,talchohol):
+        ans = ("Hello ", name,"Your Blood alchohol is:",bac,"and you have consumed ",talchohol,"drinks")
+        return ans
+
+    def quit():
+        global root
+        root.destroy()    
+    
+    def main():
+        name = application.user_name()
+        weight = float(application.get_weight())
+        gender_val = float(application.get_gender())
+        hours = float(application.time_since_last_drink())
+        b = sum(drinks)
+
         
-
-def get_weight():
-    mass=(input("How much do you weigh in pounds?: "))
-    weight=float(mass)
-    if(weight<0):
-        print("Invalid input! Weight must be greater than zero.")
-        weight=get_weight()
-    return weight
-
-def get_gender(): 
-    gender_input=str(input("What is your gender? (male or female)"))
-    if(gender_input=="male"):
-        gender=0
-    elif(gender_input=="female"):
-        gender=1
-    else:
-        print("")
-        print("Invalid input! Please input 'male' or 'female'")
-        print("")
-        gender=get_gender()
-    return gender
-
-def get_time():
-    hours=(input("How many hours has it been since your first drink?: "))
-    try:
-        time=float(hours)
-        if(time<0):
-            print("")
-            print("Invalid input! Hours must be a positive number.")
-            print("")
-            get_time()
-    except:
-        print("")
-        print("Invalid input! Hours must be a positive integer.")
-        print("")
-        get_time()
-    return time
-
-def widmark_formula(tot_drinks,weight,gender,hours):
-    if gender==1:
-        gender_val=.55
-    else:
-        gender_val=.68
-    bac=(((tot_drinks*.06*100*1.055)/(weight*gender_val))-(.015*hours))
-    return bac
-
-def advice(bac):
-    if bac>=.08:
-        print("DON'T DRIVE! You will receive a DUI and endanger the public")
-    if bac>=.10:
-        print("You're on the road to sloppiness.")
-    if bac>=.20:
-        print("You're most likely blacked out right now.")
-    if bac>=.25:
-        print("You are at a high risk of choking on your own vomit.")
-    if bac>=.35:
-        print("Coma is possible. Stop drinking.")
-    if bac>=.4:
-        print("You are about become a vegetable and may die due to respiratory arrest.")
-
-
-def receipt(drink_info,widmark_formula,name):
-    print("---------------------------------")
-    print("Hello,",name,"you have consumed:")
-    for i in range(len(drink_info)):
-        alch=drink_info[i][0]
-        amount=drink_info[i][1]
-        print(amount,'drinks of', alch)
-    print("Your Blood Alcohol Content is:",widmark_formula)
-    advice(widmark_formula)
-    print("----------------------------------")
-
-def main():
-    a=user_name()
-    b=get_content()
-    c=total_drinks(b)
-    d=get_weight()
-    e=get_gender()
-    f=get_time()
-    g=widmark_formula(c,d,e,f)
-    h=receipt(b,g,a,)
+        
+        bac=(((b*.06*100*1.055)/(weight*gender_val))-(.015*hours))
     
-main()
-          
+
+        print("--------------------------------------------------")
+        print("Hello", name,"Your Blood alchohol is:",bac)
+        for i in range(len(alch)):
+            print("You have had",drinks[i],"drinks of",alch[i])
+        print(b,"drinks in total")
+        advice = application.advice(bac)
+        print("--------------------------------------------------")
+
+
+        
+root = Tk()
+
+
+Label_title = Label(root, text="---------------------------------------------------------\nWelcome to the Regis Blood Alchohol Percentage Calculator\n---------------------------------------------------------")
+label1 = Label( root, text=" Please Enter your Name")
+label2 = Label( root, text="  Please Enter your Weight")
+label3 = Label( root, text="  Please Enter your 'Male' or 'Female'")
+label4 = Label( root, text="*********************************************************")
+label5 = Label( root, text="Please add the type of alchohol you've had and how many drinks for each type\n1 drink = 1 shot(1.5oz) or 1 beer(12oz) or 1 glass(5oz) of wine")
+label6 = Label( root, text="Please enter the Type of Alchohol")
+label7 = Label( root, text="Please enter the Number of Drinks")
+label8 = Label( root, text="Please enter how many hours ago you drank")
+
+Label_title.grid(row= 0,column= 0)
+label1.grid(row= 1, column = 0)
+label2.grid(row= 2, column = 0)
+label3.grid(row= 3, column = 0)
+label4.grid(row= 5, column = 0)
+label5.grid(row= 6, column = 0)
+label6.grid(row= 7, column = 0)
+label7.grid(row= 8, column = 0)
+label8.grid(row= 9, column = 0)
+
+e1 = Entry(root, bd =5)
+e2 = Entry(root, bd =5)
+e3 = Entry(root, bd =5)
+e4 = Entry(root, bd =5)
+e5 = Entry(root, bd =5)
+e6 = Entry(root, bd =5)
+
+e1.grid(row= 1, column = 1)
+e2.grid(row= 2, column = 1)
+e3.grid(row= 3, column = 1)
+e4.grid(row= 7, column = 1)
+e5.grid(row= 8, column = 1)
+e6.grid(row= 9, column = 1)
+
+
+
+
+
+
+add = Button(root, text = "Add", command= application.get_alchohol_list)
+add.grid(row= 8, column = 2)
+
+submit = Button(root, text ="Submit", command= application.main)
+submit.grid(row= 10, column = 1)
+x = Button(root, text = "Exit", command=application.quit)
+x.grid(row= 12, column = 1)
+
+
+
+
+
+
+
+root.mainloop()
+
